49637edffd55f0d5c82b906d03d4e3f6cedfdde3
Bibliofile <bibliofilegit@gmail.com>
2018-08-03 14:42:16 -0600

    Add info tab

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { UIExtensionExports } from '@bhmb/ui'
 // Remove for production
 import css from '../style.css'
 import { QuestsTab } from './quests-tab'
+import { InfoTab } from './info-tab'
 document.head.appendChild(document.createElement('style')).textContent = css
 
 const TAB_GROUP = 'quests'
@@ -16,6 +17,7 @@ MessageBot.registerExtension('bibliofile/quests', ex => {
 
   ui.addTabGroup('Quests', TAB_GROUP)
 
+  const infoTab = new InfoTab(ui.addTab('Info', TAB_GROUP))
   const questsTab = new QuestsTab(ex, ui, ui.addTab('Quests', TAB_GROUP))
 
   ex.remove = () => {
---
+++ src/info-tab.ts
@@ -0,0 +1,7 @@
+import html from './info-tab.html'
+
+export class InfoTab {
+  constructor (container: HTMLElement) {
+    container.innerHTML = html
+  }
+}
