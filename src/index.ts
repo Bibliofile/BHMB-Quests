d65f5a71f59431a34f0236920c4a44ce3c654205
WillQi <williamqipizza@gmail.com>
2019-10-29 01:20:08 -0600

    #1

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ MessageBot.registerExtension('bibliofile/quests', (ex, world) => {
 
   ui.addTabGroup('Quests', TAB_GROUP)
 
-  const infoTab = new InfoTab(ui.addTab('Info', TAB_GROUP))
+  const infoTab = new InfoTab(ex, ui.addTab('Info', TAB_GROUP))
   const questsTab = new QuestsTab(ex, ui, ui.addTab('Quests', TAB_GROUP))
   levelsTab(ex, ui, ui.addTab('Levels', TAB_GROUP))
   const usersTab = new UsersTab(ui.addTab('Users', TAB_GROUP), ex)
